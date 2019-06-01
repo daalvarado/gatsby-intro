dbfb36a26287a63116d15c98d419a3d0dc806836
Diego Alvarado <diego23k@gmail.com>
2019-06-01 20:22:35 +0200

    a gatsby demo site

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,3 +1,23 @@
 import React from 'react';
+import PostPreview from '../components/post-preview';
+import Hero from '../components/hero';
+import Insta from '../components/insta';
+import Layout from '../components/layout';
+import usePosts from '../hooks/use-posts';
 
-export default () => <div>Hello Frontend Masters!</div>;
+export default () => {
+  const posts = usePosts();
+
+  return (
+    <>
+      <Hero />
+      <Layout>
+        <h2>Read my blog</h2>
+        {posts.map(post => (
+          <PostPreview key={post.slug} post={post} />
+        ))}
+        <Insta />
+      </Layout>
+    </>
+  );
+};
